960fa80404b85477f887849018c5350b7945b59f
glw1900 <guoliangwei1900@gmail.com>
2015-02-02 18:30:50 -0500

    add basic label and button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var userIsInMiddleOfTyping: Bool = false
+    
+    @IBAction func digit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInMiddleOfTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInMiddleOfTyping = true
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
